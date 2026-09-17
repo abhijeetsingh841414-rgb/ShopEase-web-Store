095e6464ff8e98f972dc0cba73d63e415f94a858
Abhijeet singh <abhijeetsingh841414Qgmail.com>
2026-09-17 19:32:04 +0530

    Create search section and smooth scrooling and create new js file

---
+++ JS/products.js
@@ -0,0 +1,81 @@
+const products =  [
+    {
+        id : 1,
+        title : "Dell - Laptop",
+        category : "Electronics",
+        price : 62500,
+        rating : 4,
+        image : "image/dell-laptop3.jpg",
+        description : "Dell Laptop with fast performance, stylish design, clear display, and reliable battery life. Perfect for study, work, coding, and entertainment."
+    },
+
+    {
+        id : 2,
+        title : "Traval - Bag",
+        category : "Accessories",
+        price : 2500,
+        rating : 4,
+        image : "image/bag3.jpg",
+        description : "Durable and water-resistant backpack with dedicated laptop compartment."
+    },
+
+    {
+        id : 3,
+        title : "Dell - Laptop",
+        category : "Electronics",
+        price : 82500,
+        rating : 4.5,
+        image : "image/dell-laptop2.jpg",
+        description : "Dell Laptop with fast performance, stylish design, clear display, and reliable battery life. Perfect for study, work, coding, and entertainment."
+    },
+
+    {
+        id : 4,
+        title : "Smart Watch",
+        category : "Electronics",
+        price : 25500,
+        rating : 4.1,
+        image : "image/smart-watch1.jpg",
+        description : "Tracks heart rate, steps, sleep, and sports activities with an AMOLED display."
+    },
+
+    {
+        id : 5,
+        title : "Women Saree",
+        category : "Fashion",
+        price : 2450,
+        rating : 4.2,
+        image : "image/shari.webp",
+        description : "Saree mall Embellished sequinned Poly Georgette Saree."
+    },
+
+    {
+        id : 6,
+        title : "Cutiekins",
+        category : "Fashion",
+        price : 3458,
+        rating : 4.3,
+        image : "image/kid2.webp",
+        description : "Cutiekins Girls Bandhani Printed Angrakha Gotta Patti Kurta with Dhoti Pants."
+    },
+
+    {
+        id : 7,
+        title : "Men - PUMA-Shoes",
+        category : "Fashion",
+        price : 11456,
+        rating : 4.2,
+        image : "image/PUMA-shoes1.jpg",
+        description : "Puma shoes with a stylish design, comfortable fit, and durable sole."
+    },
+
+    {
+        id : 8,
+        title : "iPhone15",
+        category : "Mobiles",
+        price : 92500,
+        rating : 4.5,
+        image : "image/iPhone1.jpg",
+        description : "Pawerful smartphone with a premium design, high-quality camera, fast performance, and smooth user experience."
+    }
+];
--- JS/script.js
+++ JS/script.js
@@ -1,90 +1,12 @@
-const products =  [
-    {
-        id : 1,
-        title : "Dell - Laptop",
-        category : "Electronics",
-        price : 62500,
-        rating : 4,
-        image : "image/dell-laptop3.jpg",
-        description : "Dell Laptop with fast performance, stylish design, clear display, and reliable battery life. Perfect for study, work, coding, and entertainment."
-    },
-
-    {
-        id : 2,
-        title : "Traval - Bag",
-        category : "Accessories",
-        price : 2500,
-        rating : 4,
-        image : "image/bag3.jpg",
-        description : "Durable and water-resistant backpack with dedicated laptop compartment."
-    },
-
-    {
-        id : 3,
-        title : "Dell - Laptop",
-        category : "Electronics",
-        price : 82500,
-        rating : 4.5,
-        image : "image/dell-laptop2.jpg",
-        description : "Dell Laptop with fast performance, stylish design, clear display, and reliable battery life. Perfect for study, work, coding, and entertainment."
-    },
-
-    {
-        id : 4,
-        title : "Smart Watch",
-        category : "Electronics",
-        price : 25500,
-        rating : 4.1,
-        image : "image/smart-watch1.jpg",
-        description : "Tracks heart rate, steps, sleep, and sports activities with an AMOLED display."
-    },
-
-    {
-        id : 5,
-        title : "Women Saree",
-        category : "Fashion",
-        price : 2450,
-        rating : 4.2,
-        image : "image/shari.webp",
-        description : "Saree mall Embellished sequinned Poly Georgette Saree."
-    },
-
-    {
-        id : 6,
-        title : "Cutiekins",
-        category : "Fashion",
-        price : 3458,
-        rating : 4.3,
-        image : "image/kid2.webp",
-        description : "Cutiekins Girls Bandhani Printed Angrakha Gotta Patti Kurta with Dhoti Pants."
-    },
-
-    {
-        id : 7,
-        title : "Men - PUMA-Shoes",
-        category : "Fashion",
-        price : 11456,
-        rating : 4.2,
-        image : "image/PUMA-shoes1.jpg",
-        description : "Puma shoes with a stylish design, comfortable fit, and durable sole."
-    },
-
-    {
-        id : 8,
-        title : "iPhone15",
-        category : "Mobiles",
-        price : 92500,
-        rating : 4.5,
-        image : "image/iPhone1.jpg",
-        description : "Pawerful smartphone with a premium design, high-quality camera, fast performance, and smooth user experience."
-    }
-];
-
 
 const productGrid = document.getElementById("productGrid");
 const productModal = document.getElementById("product-modal");
 const closeBtn = document.querySelector(".closeBtn");
+const searchInput = document.getElementById("search-input");
+const categoryCards = document.querySelectorAll(".category-card")
 
+
+//  Rider Products in js
 function productDisplay(items){
     productGrid.innerHTML = items.map(product => {
         return `
@@ -106,12 +28,18 @@ function productDisplay(items){
               </button>
             </div>
           </div>
-        </div> `;
-    }).join('')
+        </div>
+        `;
+    }).join('');
 }
 
+
+// Products Modal and popup section
 function openModal(id){
+    console.log("Clickrd id :", id)
+    console.log("Products: ", products)
     const selectProduct = products.find(prod => prod.id === id);
+    console.log("Selecterd products: ", selectProduct);
     if(!selectProduct) return;
 
     document.getElementById("modal-img").src = selectProduct.image;
@@ -120,7 +48,7 @@ function openModal(id){
     document.getElementById("modalPrice").textContent = `₹${selectProduct.price}`;
     document.getElementById("modal-description").textContent = selectProduct.description;
 
-    productModal.style.display = "flex"
+    productModal.style.display = "flex";
 
 }
 
@@ -129,9 +57,47 @@ function addToCart(id){
     alert(`Product ${id} added to cart!`);
 }
 
-closeBtn.addEventListener('click', () => productModal.style.display = "none");
-    window.addEventListener('click', (e)=>{
-        if(e.target === productModal) productModal.style.display = "none";
-})
+closeBtn.addEventListener('click', ()=>{
+    productModal.style.display = 'none';
+});
+window.addEventListener('click', (e)=>{
+    if(e.target === productModal){
+        productModal.style.display = 'none';
+    }
+});
 
 productDisplay(products)
+
+
+//  Search bar filter logic
+
+let currentCategory = "All"
+searchInput.addEventListener('input', () =>{
+    const searchTerm = e.target.value.toLowerCase().trim();
+    filterProducts(searchTerm, currentCategory);
+})
+
+categoryCards.forEach(card =>{
+    card.addEventListener('click', ()=>{
+        currentCategory = card.getAttribute('data-category');
+
+        document.getElementById("products").scrollIntoView({behavior: 'smooth'});
+        filterProduct(searchInput.value.toLowerCase().trim(), currentCategory)
+    })
+})
+
+
+function filterProduct(searchTerm, category){
+    const filtered = products.filter(product =>{
+        const matchSearch = product.title.toLowerCase().includes(searchTerm) || product.description.toLowerCase().includes(searchTerm);
+
+        const matchCatgory = category === "All" || product.category === category;
+        return matchCatgory && matchSearch;
+    });
+
+    if(filtered.length === 0){
+        productGrid.innerHTML = `<p style ="grid-column:1/-1; text-style: center; font-size: 18px; color: #777; padding: 40px 0;">No Products Found!<p/>`;
+    }else{
+        productDisplay(filtered);
+    }
+}
